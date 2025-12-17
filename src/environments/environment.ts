93eba85915df0b6d6d16a1688824d068297bf8a0
TotallyNotTroy <tjsobrenilla@gmail.com>
2025-12-18 01:21:17 +0800

    enva

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tnapnzsdrbeaumlvhmws.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuYXBuenNkcmJlYXVtbHZobXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2ODk0NjcsImV4cCI6MjA4MDI2NTQ2N30.0SfnUrAyWqhhrtDDkPFjhoWg30_0q1RvkyduhjaWufw'
+};
